908a8448c3491c9b2c4ee9da9bed088f47524b27
stepin104310 <69225700+stepin104310@users.noreply.github.com>
2020-10-03 16:03:55 +0530

    Add files via upload

---
+++ Assessment/Assessment/BinarySerialization.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace Assessment
+{
+    [Serializable]
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public long Phone{ get; set; }
+
+        public override string ToString()
+        {
+            return string.Format($"The name: {Name} from {Address} is available at {Phone}");
+        }
+    }
+    class Serialization
+    {
+        static void Main(string[] args)
+        {
+            binaryExample();
+            Console.ReadKey();
+        }
+
+        private static void binaryExample()
+        {
+            Console.WriteLine("What do U want to do today: Read or Write");
+            string choice = Console.ReadLine();
+            if (choice.ToLower() == "read")
+                deserializing();
+            else
+                serializing();
+        }
+
+        private static void deserializing()
+        {
+            FileStream fs = new FileStream("Demo.bin", FileMode.Open, FileAccess.Read);
+            BinaryFormatter fm = new BinaryFormatter();
+            Student s = fm.Deserialize(fs) as Student;
+            Console.WriteLine(s.Name);
+            fs.Close();
+        }
+
+        private static void serializing()
+        {
+            //What to serialize:
+            Student s = new Student { Address = "Mysore", Name = "Martin", Phone = 23423423 };
+            //how to serialize:
+            BinaryFormatter fm = new BinaryFormatter();
+            //Where to serialize:
+            FileStream fs = new FileStream("Demo.bin", FileMode.OpenOrCreate, FileAccess.Write);
+            fm.Serialize(fs, s);
+            fs.Close();
+        }
+    }
+}
